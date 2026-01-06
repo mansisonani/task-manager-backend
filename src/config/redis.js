fcfec37d49751483ad63b249346fe9368400b292
mansisonani <123934021+mansisonani@users.noreply.github.com>
2026-01-06 18:57:42 +0530

    Update redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,19 +1,15 @@
 const redis = require("redis");
 
 const redisClient = redis.createClient({
-  url: "redis://127.0.0.1:6379"
+  socket: {
+    host: process.env.REDIS_HOST || "redis",
+    port: process.env.REDIS_PORT || 6379,
+  },
 });
 
-redisClient.on("connect", () => {
-  console.log("✅ Redis Connected");
-});
-
-redisClient.on("error", (err) => {
-  console.error("❌ Redis Error", err);
-});
-
-(async () => {
-  await redisClient.connect();
-})();
+redisClient
+  .connect()
+  .then(() => console.log("Redis Connected"))
+  .catch((err) => console.error("Redis Error", err));
 
 module.exports = redisClient;
